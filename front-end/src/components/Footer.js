a1a8557f8dc86dc3719506f8da777e9b63eadbf0
Chad <61545754+ChadH28@users.noreply.github.com>
2021-02-17 11:03:31 +0200

    fixed footer issue and changed styling on product page

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import './Footer.css';
-import {Link} from 'react-router-dom'
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return (
-        <footer className='footer'>
-        <div className='container'>
-            <div className="row">
-                <div className="col s4 m4 l4 footer-content"><Link to='/contact'>CONTACT US</Link></div>
-                <div className="col s4 m4 l4 footer-content"><a href='/about'>ABOUT</a></div>
-                <div className="col s4 m4 l4 footer-content"><a href='/'>COMING SOON</a></div>
-                <div className='footer-copyright col s12 m12 l12'>© 2020 By Fineprint. Made by Chad,All Ts and Cs apply</div>
-            </div>
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col s4 m4 l4 footer-content">
+            <Link to="/contact">CONTACT US</Link>
+          </div>
+          <div className="col s4 m4 l4 footer-content">
+            <a href="/about">ABOUT</a>
+          </div>
+          <div className="col s4 m4 l4 footer-content">
+            <a href="/">COMING SOON</a>
+          </div>
+          <div className="footer-copyright col s12 m12 l12">
+            © 2020 By Fineprint. Made by Chad,All Ts and Cs apply
+          </div>
         </div>
-        </footer>
-    )
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
